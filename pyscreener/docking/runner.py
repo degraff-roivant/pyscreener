023a9d9f8e2307a12cdd490739106a5c1030aefe
david graff <david.graff@roivant.com>
2022-06-08 10:32:23 -0400

    change `data` -> `sim`

--- pyscreener/docking/runner.py
+++ pyscreener/docking/runner.py
@@ -14,26 +14,26 @@ class DockingRunner(ABC):
 
     @staticmethod
     @abstractmethod
-    def prepare_receptor(data: Simulation) -> Simulation:
+    def prepare_receptor(sim: Simulation) -> Simulation:
         """Prepare the receptor file(s) for the given simulation"""
 
     @staticmethod
     @abstractmethod
-    def prepare_ligand(data: Simulation) -> bool:
+    def prepare_ligand(sim: Simulation) -> bool:
         """Prepare the ligand file(s) for the given simulation and return True upon success"""
 
     @staticmethod
     @abstractmethod
-    def run(data: Simulation) -> Optional[Sequence[float]]:
+    def run(sim: Simulation) -> Optional[Sequence[float]]:
         """Run the given simulation and return the score(s) of the docked conformers"""
 
     @staticmethod
     @abstractmethod
-    def prepare_and_run(data: Simulation) -> Optional[Result]:
+    def prepare_and_run(sim: Simulation) -> Optional[Result]:
         """Prepare the ligand file then run the given simulation. Roughly equivlaent to `prepare_ligand()` followed by `run()` but returns the Result object for the Simulation
         rather than the scores of the conformers"""
 
     @staticmethod
-    def validate_metadata(metadata: SimulationMetadata):
+    def validate_metadata(md: SimulationMetadata):
         """Validate the metadata of the simulation. E.g., ensure that the specified software is
         installed for Vina-type screens."""
